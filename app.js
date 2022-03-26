e5b90d2cccf1fee5b7abe06fac48b6ef5a7000a8
Huy Tran <tmhuyy.itiu@gmail.com>
2022-03-27 02:35:40 +0700

    set up the home page

--- app.js
+++ app.js
@@ -6,3 +6,10 @@ const app = express();
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
+
+app.get("/", function (req, res) {
+    res.render("home_page");
+});
+app.listen(3000, function () {
+    console.log("On port 3000");
+});
