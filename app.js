159c5ac01dda00e193850addbf45a73f13f5f408
Huy Tran <tmhuyy.itiu@gmail.com>
2022-03-27 02:40:55 +0700

    add some notes

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ const express = require("express");
 const mysql = require("mysql2");
 const bodyParser = require("body-parser");
 
+// TODO set up the mysql connection
 const app = express();
+// TODO set up the environment
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
 
+// TODO set up the path
 app.get("/", function (req, res) {
     res.render("home_page");
 });
