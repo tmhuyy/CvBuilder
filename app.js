3a56efde4a67bcae60d41e649f498f1960211b7e
Huy Tran <tmhuyy.itiu@gmail.com>
2022-03-27 02:17:26 +0700

    add some figures likes views and public directory

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 const express = require("express");
 const mysql = require("mysql2");
 const bodyParser = require("body-parser");
+
+const app = express();
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/public"));
