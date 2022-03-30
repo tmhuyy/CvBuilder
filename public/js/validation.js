4f256266982e09c13c2517ec91060ccad0477521
Huy Tran <tmhuyy.itiu@gmail.com>
2022-03-30 09:10:52 +0700

    define test validator variable

--- public/js/validation.js
+++ public/js/validation.js
@@ -1,6 +1,12 @@
-const validator = require("validator");
 const form = document.querySelector("#form");
+
+// TODO check empty
 const firstName = document.querySelector("#first_name");
+const lastName = document.querySelector("#last_name");
+const address = document.querySelector("#address");
+
+// TODO check email
+const email = document.querySelector("#email");
 
 form.addEventListener("click", function (e) {
     console.log("hello");
@@ -9,7 +15,7 @@ form.addEventListener("click", function (e) {
 
 const checkValidator = function () {
     const firstNameValue = firstName.value.trim();
-    console.log(validator.isEmpty(firstNameValue));
+    const lastNameValue = firstName.value.trim();
+    const addressValue = address.value.trim();
+    const emailValue = email.value.trim();
 };
-// const a = "";
-// console.log(validator.isEmpty(a));
