b2a1318855827621a2c3671ebfa6ce224b8639d0
Huy Tran <tmhuyy.itiu@gmail.com>
2022-03-31 21:54:10 +0700

    change querySelector -> querySelectorAll

--- public/js/auto_resize.js
+++ public/js/auto_resize.js
@@ -1,6 +1,8 @@
-const textarea = document.querySelector("textarea");
-textarea.addEventListener("keyup", (e) => {
-    textarea.style.height = "63px";
-    let scHeight = e.target.scrollHeight;
-    textarea.style.height = `${scHeight}px`;
-});
+const textareas = document.querySelectorAll("textarea");
+for (let textarea of textareas) {
+    textarea.addEventListener("keyup", (e) => {
+        textarea.style.height = "63px";
+        let scHeight = e.target.scrollHeight;
+        textarea.style.height = `${scHeight}px`;
+    });
+}
